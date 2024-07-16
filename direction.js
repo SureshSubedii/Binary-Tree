9b7b44624b88c84297401fe5f084fd2a11abbb01
sureshsubedii <sureshsubedi485@gmail.com>
2024-07-16 18:30:27 +0545

    Direction from start node to  destination node

--- direction.js
+++ direction.js
@@ -1,38 +1,31 @@
 var getDirections = function(root, startValue, destValue) {
-    const findPathFromRoot = (curNode, targetValue, pathToAppend) => {
-        if (!curNode) return false;
-        if (curNode.val === targetValue) return true;
-
-        pathToAppend.push("R");
-        if (findPathFromRoot(curNode.right, targetValue, pathToAppend)) return true;
-        pathToAppend.pop();
-
-        pathToAppend.push("L");
-        if (findPathFromRoot(curNode.left, targetValue, pathToAppend)) return true;
-        pathToAppend.pop();
-
-        return false;
-    };
-
-    const pathToStart = [];
-    const pathToDestination = [];
-
-    findPathFromRoot(root, startValue, pathToStart);
-    findPathFromRoot(root, destValue, pathToDestination);
-
-    let commonPathLen = 0;
-    while (commonPathLen < pathToStart.length &&
-           commonPathLen < pathToDestination.length &&
-           pathToStart[commonPathLen] === pathToDestination[commonPathLen]) {
-        commonPathLen++;
+    const findPath = (curr, target, path) =>{
+      if (!curr) return false
+      if(curr.val === target) return true
+  
+      path.push("R")
+      if(findPath(curr.right, target, path)) return true
+      path.pop()
+  
+      path.push("L")
+      if(findPath(curr.left, target, path)) return true
+      path.pop()
+  
+      return false
+  
     }
-    console.log(pathToStart, pathToDestination,commonPathLen)
-
-    const res = [];
-    for (let i = 0; i < pathToStart.length - commonPathLen; i++) {
-        res.push("U");
+    let startPath = []
+    let destPath = []
+    findPath(root, startValue, startPath)
+    findPath(root, destValue, destPath )
+    let common = 0
+    while( common < startPath.length && common < destPath.length && startPath[common] === destPath[common]) common ++
+  
+  let res = []
+    for(let i = 0; i < startPath.length - common; i++){
+      res.push("U")
     }
-    res.push(...pathToDestination.slice(commonPathLen));
-
-    return res.join("");
-};
+    res.push(...destPath.slice(common))
+    return res.join("")
+  
+  };
