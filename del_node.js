be4614a0828069c8c2bdbce4514eb11b1527c22d
sureshsubedii <sureshsubedi485@gmail.com>
2024-07-17 17:32:09 +0545

    BFS for del node

--- del_node.js
+++ del_node.js
@@ -29,3 +29,44 @@ var delNodes = function(root, to_delete) {
     return Object.values(result)
   
   };
+
+
+  // Using BFS
+
+// var delNodes = function(root, to_delete) {
+//     if (!root) return [];
+
+//     let toDeleteSet = new Set(to_delete);
+//     let result = [];
+//     let queue = [root];
+
+//     while (queue.length > 0) {
+//         let curNode = queue.shift();
+
+//         if (curNode.left) {
+//             queue.push(curNode.left);
+//             if (toDeleteSet.has(curNode.left.val)) {
+//                 curNode.left = null;
+//             }
+//         }
+
+//         if (curNode.right) {
+//             queue.push(curNode.right);
+//             if (toDeleteSet.has(curNode.right.val)) {
+//                 curNode.right = null;
+//             }
+//         }
+
+//         if (toDeleteSet.has(curNode.val)) {
+//             if (curNode.left) {
+//                 result.push(curNode.left);
+//             }
+//             if (curNode.right) {
+//                 result.push(curNode.right);
+//             }
+//         } else if (result.length === 0) {
+//             result.push(curNode);
+//         }
+//     }
+//     return result;
+// };
